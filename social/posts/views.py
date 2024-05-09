30521f1d7b73f05caef7e260c246d6ae66bd9ff0
ventil3 <v.radikov24@gmail.com>
2024-05-09 18:23:27 +0700

    Added structured main page with latest news

--- social/posts/views.py
+++ social/posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Post
+
 
 # Create your views here.
+def index(request):
+    latest = Post.objects.order_by("-pub_date")[:11]
+    return render(request, "index.html", {"posts": latest})
